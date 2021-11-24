d3be6c51588071067d07584a4cb40a66a1f2796d
Jordan <jordan@chude.co>
2021-11-24 12:20:30 -0500

    tuple example addiion

--- dist/index.js
+++ dist/index.js
@@ -7,3 +7,14 @@ let x = 'Hello World';
 let age;
 // Identifying Type of Data in Array
 let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'Hello'];
+// Tuple to Specify Types Allowed
+let person = [1, 'Jordan', true];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'Jordan'],
+    [2, 'Emily'],
+    [3, 'Daniel']
+];
+// Union (Variables to Hold More Than One Type)
--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,17 @@ let age: number
 
 // Identifying Type of Data in Array
 let ids: number[] = [1,2,3,4,5]
+let arr: any[] = [1, true, 'Hello']
+
+// Tuple to Specify Types Allowed
+let person: [number, string, boolean] = [1, 'Jordan', true];
+
+// Tuple Array
+let  employee: [number, string][]
+employee = [
+    [1, 'Jordan'],
+    [2, 'Emily'],
+    [3, 'Daniel']
+]
+
+// Union (Variables to Hold More Than One Type)
