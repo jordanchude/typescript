dc89ed52f021352f26e482470849345576c04913
Jordan <jordan@chude.co>
2021-11-24 12:15:46 -0500

    first commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// Basic Types
+let id = 5;
+let company = 'Jordan';
+let isPublished = true;
+let x = 'Hello World';
+let age;
+// Identifying Type of Data in Array
+let ids = [1, 2, 3, 4, 5];
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// Basic Types
+let id: number = 5
+let company: string = 'Jordan'
+let isPublished: boolean = true
+let x: any = 'Hello World'
+let age: number
+
+// Identifying Type of Data in Array
+let ids: number[] = [1,2,3,4,5]
